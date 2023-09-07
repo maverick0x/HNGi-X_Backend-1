8caf7bd8a9a9f8071855373ad5130edd41bd759c
Farouk Bello <mhiztahymodder@gmail.com>
2023-09-07 14:56:06 +0100

    task done..

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require("express")
-const date = new Date()
-
 const app = express()
 
 app.use(express.json())
 
 app.get('/task1', (req, res) => {
-    const { slack_name } = req.query
+    const { slack_name, track } = req.query
 
-    if (slack_name) {
+    if (slack_name && track) {
+        const date = new Date()
+
+        const full_year = date.getUTCFullYear();
+        const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+        const day = String(date.getUTCDate()).padStart(2, '0');
+        const hours = String(date.getUTCHours()).padStart(2, '0');
+        const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+        const seconds = String(date.getUTCSeconds()).padStart(2, '0');
 
         const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
         res.status(200).json({
             "slack_name": `${slack_name}`,
             "current_day": `${days[date.getUTCDay() - 1]}`,
-            "utc_time": `${date.getDate()}`
+            "utc_time": `${full_year}-${month}-${day} T${hours}:${minutes}:${seconds}Z`,
+            "track": `${track}`,
+            "github_file_url": "https://github.com/maverick0x/HNGi-X_Backend-1/blob/main/server.js",
+            "github_repo_url": "https://github.com/maverick0x/HNGi-X_Backend-1",
+            "status_code": `${res.statusCode}`
         })
     } else {
         res.status(400).json({
